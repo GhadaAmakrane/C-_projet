df94abb37e123d7c1b4abd315b88838e2dd976f1
GhadaAmakrane <ghada.amakrane1@gmail.com>
2024-01-09 11:12:18 +0100

    Constructeur de recopie

--- Livre.cpp
+++ Livre.cpp
@@ -25,6 +25,36 @@ Livre :: Livre(string titre, string code, string ISBN) {
 
 }
 
+Livre:: Livre(Livre* livre) {
+        if (livre != nullptr) {
+            code = livre->code;
+            auteur = livre->auteur;
+            titre = livre->titre;
+            editeur = livre->editeur;
+            ISBN = livre->ISBN;
+            publicCible = livre->publicCible;
+            etat = livre->etat;
+
+            // Copie profonde du pointeur Bibliotheque
+            if (livre->bibliotheque_origine != nullptr) {
+                bibliotheque_origine = new Bibliotheque(*livre->bibliotheque_origine);
+            } else {
+                bibliotheque_origine = nullptr;
+            }
+        } else {
+            // Si le pointeur est nul, initialiser les attributs à des valeurs par défaut
+            code = "";
+            auteur = "";
+            titre = "";
+            editeur = "";
+            ISBN = "";
+            publicCible = "";
+            etat = "";
+            bibliotheque_origine = nullptr;
+        }
+    }
+
+
 string Livre::getCode() {
     return code;
 }
