2ee311e5cfd39a38f4b2ecfa880190706463b0b6
estebancarlin <122169301+estebancarlin@users.noreply.github.com>
2024-01-09 10:35:47 +0100

    Update BD.cpp

--- BD.cpp
+++ BD.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-
-using namespace std;
 #include "BD.h"
 
 
-// Constructeur prenant les attributs spécifiques et appelant le constructeur de Livre
-BD::BD(string titre, string code, string ISBN, string dessinateur)
+#include <iostream>
+using namespace std;
+
+BD::BD(string titre, string code,string ISBN, string dessinateur)
     : Livre(titre, code, ISBN), dessinateur(dessinateur) {}
 
-// Constructeur de recopie
+
 BD::BD(Livre livre, string dessinateur)
     : Livre(livre), dessinateur(dessinateur) {}
 
-// Getter pour le genre
+
+// Getter
 string BD::getDessinateur() {
     return dessinateur;
 }
 
-// Setter pour le genre
+// Setter
 void BD::setDessinateur(string nouveauDessinateur) {
 	dessinateur = nouveauDessinateur;
 }
