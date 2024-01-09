e81268c0da4a6ceccc3fb3c4afc330a0d82d70e6
AfekAmiri <94656927+AfekAmiri@users.noreply.github.com>
2024-01-09 10:31:30 +0100

    Update ListeLivres.h

--- ListeLivres.h
+++ ListeLivres.h
@@ -1,1 +1,17 @@
+#ifndef __LISTELIVRES_H__
+#define __LISTELIVRES_H__
 
+#include <iostream>
+using namespace std;
+#include "Noeud.h"
+
+class ListeLivres {
+    private:
+        Noeud *tete;  // Pointeur vers la tete
+        Noeud *suivant;  // Pointeur vers le prochain nœud dans la liste
+    public:
+        ListeLivres(); // Constructeur par défaut
+        ListeLivres(Livre);
+};
+
+#endif // __LISTELIVRES_H__
