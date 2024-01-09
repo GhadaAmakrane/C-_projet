9c261c6b5cd5c8c06300000e88fd3bea419a750d
estebancarlin <122169301+estebancarlin@users.noreply.github.com>
2024-01-09 10:24:41 +0100

    BD

---
+++ BD.h
@@ -0,0 +1,20 @@
+#ifndef __BD_H__
+#define __BD_H__
+
+#include <iostream>
+using namespace std;
+#include "Livre.h"
+
+class BD:
+		public Livre {
+    private:
+        string dessinateur ;
+    public:
+        BD(string,string,string,string);
+        BD(Livre*,string); // Recopie
+
+        string getDessinateur(); //getter
+        void setDessinateur(string); //setter
+};
+
+#endif // __BD_H__
