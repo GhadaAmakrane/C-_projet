c737cda4a6f17346f546858e86585d7858db7e5b
estebancarlin <122169301+estebancarlin@users.noreply.github.com>
2024-01-20 23:28:47 +0100

    Update Receuil_Poesie.cpp

--- Receuil_Poesie.cpp
+++ Receuil_Poesie.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+#include "Recueil_Poesie.h"
 
+
+// Constructeur prenant les attributs spécifiques et appelant le constructeur de Livre
+Roman::Roman(string titre, string code, string ISBN, string inducateurVersProse)
+    : Livre(titre, code, ISBN), genre(genre) {}
+
+// Constructeur de recopie
+Roman::Roman(Livre* livre,string inducateurVersProse) {
+    Livre(livre);
+    inducateurVersProse = inducateurVersProse;
+}
+
+// Getter pour le genre
+string Roman::getInducateurVersProse() {
+    return inducateurVersProse;
+}
+
+// Setter pour le genre
+void Roman::setInducateurVersProse(string nouveauInducateurVersProse) {
+    inducateurVersProse = nouveauInducateurVersProse;
+}
