b3e2d6991a7046959401f6b582c79bce80964967
estebancarlin <122169301+estebancarlin@users.noreply.github.com>
2024-01-21 01:26:36 +0100

    Update Roman.cpp

--- Roman.cpp
+++ Roman.cpp
@@ -1,28 +1,21 @@
-#ifndef __RECUEIL_POESIE_H__
-#define __RECUEIL_POESIE_H__
-
 #include <iostream>
 using namespace std;
-#include "Livre.h"
-
-class Recueil_Poesie : public Livre {
-    private:
-        string inducateurVersProse;
-
-    public:
-
-        Recueil_Poesie(string,string,string,string);
-        Recueil_Poesie(Livre*,string); // Constrcteur de recopie
-
-        //getter
-
-        string getInducateurVersProse();
-
-        //setter
-
-        void setInducateurVersProse(string);
-
-};
+#include "Roman.h"
 
 
-#endif // __RECUEIL_POESIE_H__
+// Constructeur prenant les attributs spécifiques et appelant le constructeur de Livre
+Roman::Roman(string titre, string code, string ISBN, string genre)
+    : Livre(titre, code, ISBN), genre(genre) {}
+
+Roman::Roman(Livre* livre, string genre)
+    : Livre(livre), genre(genre) {}
+
+// Getter pour le genre
+string Roman::getGenre() {
+    return genre;
+}
+
+// Setter pour le genre
+void Roman::setGenre(string nouveauGenre) {
+    genre = nouveauGenre;
+}
