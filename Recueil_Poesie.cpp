f29cadc086432cd1d84f3b30528a89a80a414e82
estebancarlin <122169301+estebancarlin@users.noreply.github.com>
2024-01-25 19:12:37 +0100

    Update Recueil_Poesie.cpp

--- Recueil_Poesie.cpp
+++ Recueil_Poesie.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 
 // Constructeur prenant les attributs spécifiques et appelant le constructeur de Livre
-Recueil_Poesie::Recueil_Poesie(string titre, string code, string ISBN, string inducateurVersProse)
-    : Livre(titre, code, ISBN), inducateurVersProse(inducateurVersProse) {}
+Recueil_Poesie::Recueil_Poesie(string titre, string code, string ISBN, string indicateurVersProse)
+    : Livre(titre, code, ISBN), indicateurVersProse(indicateurVersProse) {}
 
 
-Recueil_Poesie::Recueil_Poesie(Livre* livre, string inducateurVersProse)
-    : Livre(livre), inducateurVersProse(inducateurVersProse) {}
+Recueil_Poesie::Recueil_Poesie(Livre* livre, string indicateurVersProse)
+    : Livre(livre), indicateurVersProse(indicateurVersProse) {}
 
 // Getter pour
-string Recueil_Poesie::getInducateurVersProse() {
-    return inducateurVersProse;
+string Recueil_Poesie::getIndicateurVersProse() {
+    return indicateurVersProse;
 }
 
 // Setter pour
-void Recueil_Poesie::setInducateurVersProse(string nouveauInducateurVersProse) {
-    inducateurVersProse = nouveauInducateurVersProse;
+void Recueil_Poesie::setIndicateurVersProse(string nouveauIndicateurVersProse) {
+    indicateurVersProse = nouveauIndicateurVersProse;
 }
